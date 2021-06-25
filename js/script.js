200fdf83ef5a3921a9269911c2c2d638f2fbc723
l1ghbe <ee1zet25@gmail.com>
2021-06-25 20:22:13 +0300

    added titles

--- js/script.js
+++ js/script.js
@@ -42,50 +42,95 @@ window.addEventListener('mousemove', e => {
 
 const tlText = gsap.timeline({defaults: {duration: 1, ease: 'Bounce.easeOut'}})
 
-tlText.from(text1, {scale: 0, delay: .4})
-      .from(text2, {scale: 0})
-      .from(text3, {scale: 0})
+window.addEventListener('load', () => {
+    tlText.from(text1, { scale: 0, delay: .4 })
+        .from(text2, { scale: 0 })
+        .from(text3, { scale: 0 })
 
-gsap.from('.logo', {
-    duration: 1,
-    opacity: 0,
-    y: -150,
-    delay: 3
-})
-gsap.from('.navigation-item', {
-    duration: 1.5,
-    opacity: 0,
-    y: -150,
-    stagger: .1,
-    delay: 3.1
-})
-gsap.from('.main-links', {
-    duration: 1,
-    y: '100%',
-    delay: 3.5,
-    ease: 'easeOut',
-
-})
-if (window.innerHeight < 642) {
+    gsap.from('.logo', {
+        duration: 1,
+        opacity: 0,
+        y: -150,
+        delay: 3
+    })
+    gsap.from('.navigation-item', {
+        duration: 1.5,
+        opacity: 0,
+        y: -150,
+        stagger: .1,
+        delay: 3.1
+    })
     gsap.from('.main-links', {
         duration: 1,
-        x: '-150%',
+        y: '100%',
         delay: 3.5,
         ease: 'easeOut',
 
     })
-}
-gsap.from('.main-description-text', {
-    duration: 1,
-    x: -150,
-    delay: 3.6,
-    opacity: 0
+    if (window.innerHeight < 642) {
+        gsap.from('.main-links', {
+            duration: 1,
+            x: '-150%',
+            delay: 3.5,
+            ease: 'easeOut',
+
+        })
+    }
+    gsap.from('.main-description-text', {
+        duration: 1,
+        x: -150,
+        delay: 3.6,
+        opacity: 0
+    })
+    gsap.from('.image-parallax', {
+        duration: 1,
+        opacity: 0,
+        delay: 4,
+        stagger: 0.3,
+        y: 50
+    })
 })
-gsap.from('.image-parallax', {
-    duration: 1,
-    opacity: 0,
-    delay: 4,
-    stagger: 0.3,
-    y: 50
-})
+
+// gsap.from('.logo', {
+//     duration: 1,
+//     opacity: 0,
+//     y: -150,
+//     delay: 3
+// })
+// gsap.from('.navigation-item', {
+//     duration: 1.5,
+//     opacity: 0,
+//     y: -150,
+//     stagger: .1,
+//     delay: 3.1
+// })
+// gsap.from('.main-links', {
+//     duration: 1,
+//     y: '100%',
+//     delay: 3.5,
+//     ease: 'easeOut',
+
+// })
+// if (window.innerHeight < 642) {
+//     gsap.from('.main-links', {
+//         duration: 1,
+//         x: '-150%',
+//         delay: 3.5,
+//         ease: 'easeOut',
+
+//     })
+// }
+// gsap.from('.main-description-text', {
+//     duration: 1,
+//     x: -150,
+//     delay: 3.6,
+//     opacity: 0
+// })
+// gsap.from('.image-parallax', {
+//     duration: 1,
+//     opacity: 0,
+//     delay: 4,
+//     stagger: 0.3,
+//     y: 50
+// })
         
